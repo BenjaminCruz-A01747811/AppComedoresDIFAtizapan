4b3b883c1e0cdd8503364e165f6673fccb16cc57
SopitaNipona <a01748692@tec.mx>
2023-10-22 18:22:16 -0600

    Interraciones con firebase al momento de registrar o iniciar sesion de usuario y colaboradores

--- app/src/main/java/mx/tec/bacc/appcomedoresdifatizapan/ui/interfaces/colaborador/InicioColabFragment.kt
+++ app/src/main/java/mx/tec/bacc/appcomedoresdifatizapan/ui/interfaces/colaborador/InicioColabFragment.kt
@@ -10,10 +10,64 @@ import androidx.fragment.app.Fragment
 import mx.tec.bacc.appcomedoresdifatizapan.R
 import mx.tec.bacc.appcomedoresdifatizapan.databinding.FragmentInicioColaboradorBinding
 import mx.tec.bacc.appcomedoresdifatizapan.databinding.FragmentRegistroColaboradorBinding
+import mx.tec.bacc.appcomedoresdifatizapan.ui.interfaces.colaborador.Colaborador
+import com.google.firebase.firestore.FirebaseFirestore
+
+object colaboradorIni{
+    var nombre: String = ""
+    var apellidos: String = ""
+    var fechaNac: String = ""
+    var sexo: String = ""
+    var contraseña: String = ""
+    var correo: String = ""
+    var notNull: Boolean = true
+
+
+}
 
 class InicioColabFragment: Fragment() {
     private val binding get() = _binding!!
     private var _binding: FragmentInicioColaboradorBinding? = null
+    val db = FirebaseFirestore.getInstance()
+
+    fun searchUsuarioByEmail(targetEmail: String, targetContraseña: String, callback: (Colaborador?) -> Unit) {
+        val usuariosCollection = db.collection("colaboradores")
+
+        try {
+            usuariosCollection.whereEqualTo("correo", targetEmail) // Change to email
+                .whereEqualTo("contraseña", targetContraseña)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    for (document in querySnapshot.documents) {
+                        val data = document.data
+                        if (data != null) {
+                            val colaborador = Colaborador(
+                                data["nombre"] as String,
+                                data["apellidos"] as String,
+                                data["fechaNac"] as String,
+                                data["sexo"] as String,
+                                data["contraseña"] as String,
+                                data["correo"] as String // Retrieve email
+                            )
+                            callback(colaborador)
+                            return@addOnSuccessListener
+                        }
+                    }
+
+                    // If no matching document is found, return null
+                    callback(null)
+                }
+                .addOnFailureListener { exception ->
+                    // Handle the exception here if necessary
+                    callback(null)
+                }
+        } catch (e: Exception) {
+            callback(null)
+        }
+    }
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +86,25 @@ class InicioColabFragment: Fragment() {
         btnIngresar.setOnClickListener{
             val correoString = etCorreo.text.toString().trim()
             val contraString = etContra.text.toString().trim()
+
+            val foundColaborador = searchUsuarioByEmail(correoString, contraString) { colaborador ->
+                // Handle the result here
+                if (colaborador != null) {
+                    // A matching user was found
+                    // Use the `colaborador` object
+                    colaboradorIni.nombre = colaborador.nombre
+                    colaboradorIni.apellidos = colaborador.apellidos
+                    colaboradorIni.correo = colaborador.correo // Set email
+                    colaboradorIni.fechaNac = colaborador.fechaNac
+                    colaboradorIni.sexo = colaborador.sexo
+                    colaboradorIni.contraseña = colaborador.contraseña
+                    colaboradorIni.notNull = true
+                } else {
+                    // No matching user found
+                    println("User not found.")
+                    colaboradorIni.notNull = false
+                }
+            }
         }
 
         // Cambiar a Registro Colaborador
--- app/src/main/java/mx/tec/bacc/appcomedoresdifatizapan/ui/interfaces/colaborador/RegistroColabFragment.kt
+++ app/src/main/java/mx/tec/bacc/appcomedoresdifatizapan/ui/interfaces/colaborador/RegistroColabFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import mx.tec.bacc.appcomedoresdifatizapan.R
 import mx.tec.bacc.appcomedoresdifatizapan.databinding.FragmentInicioColaboradorBinding
 import mx.tec.bacc.appcomedoresdifatizapan.databinding.FragmentRegistroColaboradorBinding
+import mx.tec.bacc.appcomedoresdifatizapan.ui.interfaces.colaborador.Colaborador
+import com.google.firebase.firestore.FirebaseFirestore
+import java.time.format.DateTimeFormatter
 
 class RegistroColabFragment: Fragment() {
 
     private val binding get() = _binding!!
     private var _binding: FragmentRegistroColaboradorBinding? = null
+    private val db = FirebaseFirestore.getInstance()
+
+    fun getCurrentDate(): String {
+        val currentDate = java.time.LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        return currentDate.format(formatter)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +41,46 @@ class RegistroColabFragment: Fragment() {
         val etContra = binding.edtContra3
         val btnRegColab = binding.btnRegColab
 
+        val currentDate = getCurrentDate()
+        val usuariosCollection = db.collection("colaboradores")
+
         btnRegColab.setOnClickListener{
             val correoString = etNombre.text.toString().trim()
             val nombreString = etApellidos.text.toString().trim()
             val contraString = etContra.text.toString().trim()
+
+            if (nombreString.isEmpty() || correoString.isEmpty() || contraString.isEmpty() ) {
+                Toast.makeText(context, "Complete los datos", Toast.LENGTH_SHORT).show()
+            } else {
+                // Check if the "curp" is duplicated before adding the document
+                checkCurpDuplication(correoString) {
+                    if (it) {
+                        // Handle the case when "curp" is duplicated
+                        println("Duplicated CURP: $correoString. Cannot add the document.")
+                    } else {
+                        // "curp" is not duplicated, so add the document
+                        val nuevoUsuario = hashMapOf(
+                            "nombre" to nombreString,
+                            "correo" to correoString,
+                            "contraseña" to contraString,
+                            "fechaRegistro" to currentDate // Add registration date
+                        )
+
+                        usuariosCollection.add(nuevoUsuario)
+                            .addOnSuccessListener {
+                                // Documento creado con éxito
+                                println("Documento creado con éxito")
+                                // Redirect to the desired screen or show a success message.
+                            }
+                            .addOnFailureListener { e ->
+                                // Error al crear el documento
+                                println("Error al crear el documento: $e")
+                                // Handle the error, show an error message, etc.
+                            }
+                    }
+                }
+            }
+
         }
 
         // Cambiar a Inicio Colaborador
@@ -46,5 +93,17 @@ class RegistroColabFragment: Fragment() {
         return root
     }
 
+    private fun checkCurpDuplication(newCurp: String, callback: (Boolean) -> Unit) {
+        val collectionRef = db.collection("usuarios")
+
+        collectionRef.whereEqualTo("correo", newCurp).get()
+            .addOnSuccessListener { querySnapshot ->
+                callback(!querySnapshot.isEmpty)
+            }
+            .addOnFailureListener { e ->
+                callback(false)
+                println("Error al verificar duplicado CURP: $e")
+            }
+    }
 
 }
